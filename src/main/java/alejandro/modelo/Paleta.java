edcb39e5231c99670e09c1c4cc371b6e96285295
Alejandro Mendez <alemendez1223@gmail.com>
2025-06-28 14:59:10 -0300

    feat: implementar Paleta.java

--- src/main/java/alejandro/modelo/Paleta.java
+++ src/main/java/alejandro/modelo/Paleta.java
@@ -1,4 +1,26 @@
 package alejandro.modelo;
 
-public class Paleta {
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Point2D;
+
+public class Paleta extends Dimension2D {
+    private static final double WIDTH = 30;
+    private static final double HEIGHT = 70;
+    private Point2D posicion;
+    private Velocidad velocidad; // ver de usar solo un double -> Vel vertical
+
+    public Paleta(Point2D posicion) {
+        super(WIDTH, HEIGHT);
+        this.posicion = posicion;
+    }
+
+    public Point2D getPosicion() {
+        return posicion;
+    }
+
+    public Velocidad getVelocidad() {
+        return velocidad;
+    }
+
+    // mover()
 }
